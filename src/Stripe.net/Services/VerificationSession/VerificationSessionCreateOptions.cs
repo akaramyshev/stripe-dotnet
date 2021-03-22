f329730d125d03a3371ddf95bca06640c3e1f281
akaramyshev <akaramyshev@aexsoft.com>
2021-03-22 14:48:44 +0700

    Add metadata to options

--- src/Stripe.net/Services/VerificationSession/VerificationSessionCreateOptions.cs
+++ src/Stripe.net/Services/VerificationSession/VerificationSessionCreateOptions.cs
@@ -1,5 +1,6 @@
 ﻿namespace Stripe
 {
+    using System.Collections.Generic;
     using Newtonsoft.Json;
 
     public class VerificationSessionCreateOptions : BaseOptions
@@ -27,5 +28,14 @@
         /// </summary>
         [JsonIgnore]
         public string StripeVersion { get; set; }
+
+        /// <summary>
+        /// Set of <a href="https://stripe.com/docs/api/metadata">key-value pairs</a> that you can
+        /// attach to an object. This can be useful for storing additional information about the
+        /// object in a structured format. Individual keys can be unset by posting an empty value to
+        /// them. All keys can be unset by posting an empty value to <c>metadata</c>.
+        /// </summary>
+        [JsonProperty("metadata")]
+        public Dictionary<string, string> Metadata { get; set; }
     }
 }
